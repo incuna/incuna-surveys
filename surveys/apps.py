4bb0a4b990fbbc6b38b76666ddf13d68c6e706a4
Adam Thomas <sortoflikechess@gmail.com>
2016-08-09 16:20:01 +0100

    Add a way for a SurveyField to generate a form field.

--- surveys/apps.py
+++ surveys/apps.py
@@ -3,6 +3,14 @@ from collections import OrderedDict
 from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
+from .form_field_generators import (
+    CharFieldGenerator,
+    MultipleChoiceFieldGenerator,
+    NumberFieldGenerator,
+    PercentageFieldGenerator,
+    RadioFieldGenerator,
+)
+
 
 class SurveyConfig(AppConfig):
     name = 'surveys'
@@ -13,27 +21,27 @@ class SurveyConfig(AppConfig):
     field_type_data = OrderedDict((
         ('free_text', {
             'display_text': _('Character field (free text)'),
-            'form_field_factory': None,
+            'form_field_factory': CharFieldGenerator,
             'serializer_field_factory': None,
         }),
         ('number', {
             'display_text': _('Number'),
-            'form_field_factory': None,
+            'form_field_factory': NumberFieldGenerator,
             'serializer_field_factory': None,
         }),
         ('percentage', {
             'display_text': _('Percentage'),
-            'form_field_factory': None,
+            'form_field_factory': PercentageFieldGenerator,
             'serializer_field_factory': None,
         }),
         ('radio', {
             'display_text': _('Choose one option'),
-            'form_field_factory': None,
+            'form_field_factory': RadioFieldGenerator,
             'serializer_field_factory': None,
         }),
         ('checkbox', {
             'display_text': _('Choose one or more options'),
-            'form_field_factory': None,
+            'form_field_factory': MultipleChoiceFieldGenerator,
             'serializer_field_factory': None,
         }),
     ))
@@ -46,9 +54,9 @@ class SurveyConfig(AppConfig):
         # Choices suitable for the field_type dropdown on SurveyField.
         self.field_type_choices = []
         # Convenient mappings of field types to form/serializer field factories.
-        self.form_field_factories = []
-        self.serializer_field_factories = []
+        self.form_field_factories = {}
+        self.serializer_field_factories = {}
         for k, v in field_data_items:
             self.field_type_choices.append((k, v['display_text']))
-            self.form_field_factories.append((k, v['form_field_factory']))
-            self.serializer_field_factories.append((k, v['serializer_field_factory']))
+            self.form_field_factories[k] = v['form_field_factory']
+            self.serializer_field_factories[k] = v['serializer_field_factory']
