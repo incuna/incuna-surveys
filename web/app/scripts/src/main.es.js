2231d1d714d054724185d2d9a41ef9c7d27dc1d2
wytrych <fuzzzym@gmail.com>
2016-08-10 12:22:06 +0200

    experimenting with angular and es6 modules

--- web/app/scripts/src/main.es.js
+++ web/app/scripts/src/main.es.js
@@ -1,5 +1,8 @@
 /* global console */
 // TODO: remove the following two lines
+console.log(window.angular);
+console.log('dupa');
 import { myObj } from 'test';
 /* eslint no-console: 0 */
 console.log(myObj);
+console.log(angular);
--- web/app/scripts/src/test.es.js
+++ web/app/scripts/src/test.es.js
@@ -1,5 +1,6 @@
 // TODO: remove this file, it is only proof of concept
 
+import angular from 'lib-npm/angular/index';
 export var myObj = {
     one: 1,
     two: 2
