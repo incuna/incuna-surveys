eac6afa4b66a7ffe01e3eca3b502b1972e7791c7
Adam Thomas <sortoflikechess@gmail.com>
2016-08-12 16:10:49 +0100

    Add and test a survey GET view.

--- surveys/tests/utils.py
+++ surveys/tests/utils.py
@@ -1,7 +1,27 @@
 from incuna_test_utils.testcases.request import BaseRequestTestCase
 
-from .factories import UserFactory
+from . import factories
 
 
 class RequestTestCase(BaseRequestTestCase):
-    user_factory = UserFactory
+    user_factory = factories.UserFactory
+
+
+def create_survey_data(cls):
+    """
+    For use in setUpTestData to build a survey.
+
+    The survey has one fieldset, which has one field.
+    """
+    cls.field = factories.SurveyFieldFactory.create(field_type='number')
+    cls.fieldset = factories.SurveyFieldsetFactory.create()
+    cls.survey = factories.SurveyFactory.create()
+
+    factories.SurveyFieldOrderingFactory.create(
+        field=cls.field,
+        fieldset=cls.fieldset,
+    )
+    factories.SurveyFieldsetOrderingFactory.create(
+        survey=cls.survey,
+        fieldset=cls.fieldset,
+    )
