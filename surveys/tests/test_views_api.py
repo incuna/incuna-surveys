d0df2d9b55ae7645e0956df3c79cb15e5cb3c500
Adam Thomas <sortoflikechess@gmail.com>
2016-08-17 15:00:56 +0100

    Add and test a POST view.

--- surveys/tests/test_views_api.py
+++ surveys/tests/test_views_api.py
@@ -1,9 +1,10 @@
-from .utils import create_survey_data, RequestTestCase
+from .utils import APIRequestTestCase, create_survey_data
 from .. import views_api
+from ..models import UserResponse
 from ..serializers import SurveySerializer
 
 
-class TestSurveyView(RequestTestCase):
+class TestSurveyViews(APIRequestTestCase):
     @classmethod
     def setUpTestData(cls):
         create_survey_data(cls)
@@ -25,3 +26,32 @@ class TestSurveyView(RequestTestCase):
             context={'request': request},
         ).data
         self.assertEqual(response.data, expected_data)
+
+    def test_post(self):
+        """Test that the view can retrieve a survey and return correct JSON."""
+        view = views_api.SurveyPostView.as_view()
+        data = {
+            'user_id': 'User#20#',
+            'user_responses': [
+                {
+                    'fieldset': 1,
+                    'answers': {'1': 42}
+                }
+            ],
+        }
+
+        request = self.create_request(method='post', data=data)
+        response = view(request, pk=self.survey.pk)
+
+        self.assertEqual(response.status_code, 201, response.data)
+
+        # Assert we return the same data but with the survey and user_id re-specified
+        # on the objects that need them.
+        data['survey'] = self.survey.pk
+        data['user_responses'][0]['user_id'] = data['user_id']
+        self.assertEqual(response.data, data)
+
+        # Assert the UserResponse object was created correctly.
+        user_response = UserResponse.objects.first()
+        self.assertEqual(user_response.user_id, data['user_id'])
+        self.assertEqual(user_response.answers, data['user_responses'][0]['answers'])
