6096b9a8fcfb98466e07de3091cba1a0eef8634f
wytrych <fuzzzym@gmail.com>
2016-08-18 16:47:22 +0200

    Cleanup test-app
    Move test.es.js out of src/
    Load the api desc into test-app
    Form test.es.js correctly

---
+++ web/app/scripts/test-app/test.es.js
@@ -0,0 +1,28 @@
+(function (angular) {
+    
+    const module = angular.module('test-app', ['incuna-surveys']);
+
+    module.controller('FormCtrl', [
+        'formlyConfig',
+        'FieldsParser',
+        '$http',
+        function (
+            formlyConfig,
+            FieldsetParserService,
+            $http
+        ) {
+
+            $http({
+                method: 'GET',
+                url: '/scripts/src/get.json'
+            }).then((res) => {
+                const form = res.data.OK.response_data;
+
+                this.fields = FieldsetParserService.parseFields(form);
+                this.model = FieldsetParserService.parseModel(form);
+            });
+
+        }
+    ]);
+
+}(window.angular));
--- web/dist/incuna-forms.js
+++ web/dist/incuna-forms.js
@@ -299,69 +299,23 @@ exports.default = moduleProperties;
 },{"./../libraries.js":2,"./fields-config.js":6}],8:[function(require,module,exports){
 'use strict';
 
-var apiDesc = {
-    /* jscs:disable disallowQuotedKeysInObjects */
-    'name': 'How have you been using the site?',
-    'description': 'Form description',
-    'url': 'http://localhost:8000/forms/1',
-    'fieldsets': [{
-        'id': 1,
-        'name': 'Free text field',
-        'description': 'Some additional text expanding on the above',
-        'fields': [{
-            'id': 1,
-            'name': 'How did you discover the site?',
-            'help_text': 'Search engine, friend...',
-            'field_type': 'free_text',
-            'required': true,
-            'answers': []
-        }]
-    }, {
-        'id': 2,
-        'name': 'Numeric fields',
-        'description': 'Some additional text expanding on the above',
-        'fields': [{
-            'id': 2,
-            'name': 'What is answer to the ultimate question of life, the universe, and everything?',
-            'help_text': null,
-            'field_type': 'number',
-            'required': false,
-            'answers': []
-        }, {
-            'id': 3,
-            'name': 'How much percentage does it take?',
-            'help_text': null,
-            'field_type': 'percentage',
-            'required': true,
-            'answers': []
-        }]
-    }, {
-        'id': 3,
-        'name': 'Choice fields',
-        'description': 'Some additional text expanding on the above',
-        'fields': [{
-            'id': 4,
-            'name': 'How many time do you visit the site per day?',
-            'help_text': 'Choose a valid answer.',
-            'field_type': 'radio',
-            'required': true,
-            'answers': ['One time', 'Two times', 'Three times or more']
-        }, {
-            'id': 5,
-            'name': 'Choose more than one answer.',
-            'help_text': null,
-            'field_type': 'checkbox',
-            'required': false,
-            'answers': ['One time', 'Two times', 'Three times or more']
-        }]
-    }]
-    /* jscs:enable disallowQuotedKeysInObjects */
-};
+(function (angular) {
 
-angular.module('test-app', ['incuna-surveys']).controller('FormCtrl', ['formlyConfig', 'FieldsParser', function (formlyConfig, FieldsetParserService) {
+    var module = angular.module('test-app', ['incuna-surveys']);
 
-    this.fields = FieldsetParserService.parseFields(apiDesc);
-    this.model = FieldsetParserService.parseModel(apiDesc);
-}]);
+    module.controller('FormCtrl', ['formlyConfig', 'FieldsParser', '$http', function (formlyConfig, FieldsetParserService, $http) {
+        var _this = this;
+
+        $http({
+            method: 'GET',
+            url: '/scripts/src/get.json'
+        }).then(function (res) {
+            var form = res.data.OK.response_data;
+
+            _this.fields = FieldsetParserService.parseFields(form);
+            _this.model = FieldsetParserService.parseModel(form);
+        });
+    }]);
+})(window.angular);
 
 },{}]},{},[1,2,3,4,5,6,7,8]);
