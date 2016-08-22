c58d551124b6d8f62a5cc2b763ad518d95d83fea
Adam Thomas <sortoflikechess@gmail.com>
2016-08-22 10:10:36 +0100

    Add URL for SurveyPostView.

--- surveys/urls.py
+++ surveys/urls.py
@@ -5,4 +5,9 @@ from . import views_api
 
 urlpatterns = [
     url(r'^forms/(?P<pk>\d+)/?$', views_api.SurveyView.as_view(), name='survey-form'),
+    url(
+        r'^forms/(?P<pk>\d+)/respond/?$',
+        views_api.SurveyPostView.as_view(),
+        name='survey-post',
+    ),
 ]
