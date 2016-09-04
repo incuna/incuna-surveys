87c2596dfe91bc166f0892f7704b37a3d2e3fdf6
Max Peterson <max@incuna.com>
2016-09-04 21:59:39 +0100

    Update orderable to 4.0.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'django-orderable==4.0.3',
+        'django-orderable==4.0.4',
     ],
     description=(
         'Allows the creation of custom questionnaires and surveys via the Django admin.'
