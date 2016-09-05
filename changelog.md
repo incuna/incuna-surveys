154cc667d5ae49863f39d189b8aaa9a22d107e3f
Max Peterson <max@incuna.com>
2016-09-05 10:11:20 +0100

    Bump to 0.2.1

--- changelog.md
+++ changelog.md
@@ -2,7 +2,7 @@
 
 This project uses Semantic Versioning (2.0).
 
-### UPCOMING
+### 0.2.1
 
 * Add default `sort_order` ordering to Orderable models.
 * Add admin fields and filtering.
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 
 setup(
-    version='0.2.0',
+    version='0.2.1',
     name='incuna-surveys',
     packages=find_packages(),
     include_package_data=True,
