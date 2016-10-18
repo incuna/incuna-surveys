dc5e5af0054424c2c378327318c817342f8d416e
todd-dembrey <toddd@incuna.com>
2016-10-18 15:59:04 +0100

    update admin and settings

--- test_project/settings.py
+++ test_project/settings.py
@@ -1,7 +1,7 @@
 import os
 
 import dj_database_url
-
+from django.utils.translation import ugettext_lazy as _
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 API_DESCRIPTION_DIR = os.path.join(BASE_DIR, 'api-description')
@@ -25,6 +25,7 @@ INSTALLED_APPS = (
     'surveys.tests',
 
     'orderable',
+    'parler',
     'rest_framework',
 
     'corsheaders',
@@ -70,3 +71,19 @@ TEST_RUNNER = 'test_project.test_runner.Runner'
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
+
+LANGUAGE_CODE = 'en'
+
+LANGUAGES = (
+    ('en', _("English")),
+    ('fr', _("French")),
+    ('es', _("Spanish")),
+)
+
+PARLER_LANGUAGES = {
+    None: (
+        {'code': 'en'},
+        {'code': 'fr'},
+        {'code': 'es'},
+    ),
+}
